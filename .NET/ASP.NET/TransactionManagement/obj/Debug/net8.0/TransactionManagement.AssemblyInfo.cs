4389e57d18af930f8fd837c3d388a54abd7ac73b
Saurav Dwivedi <saurav.dwivedi@gmail.com>
2024-10-12 11:44:27 +0200

    Remove boilerplate crap

--- .NET/ASP.NET/TransactionManagement/obj/Debug/net8.0/TransactionManagement.AssemblyInfo.cs
+++ .NET/ASP.NET/TransactionManagement/obj/Debug/net8.0/TransactionManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransactionManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc771be477c436ec7f9b0baef62bc887632cdca9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45910f072910035430ce04cf1457d6709b1aee99")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransactionManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransactionManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
