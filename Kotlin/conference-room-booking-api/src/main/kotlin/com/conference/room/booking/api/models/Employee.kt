a59128ee878dc61326daab28050d96ca98647886
Saurav Dwivedi <saurav.dwivedi@gmail.com>
2024-10-29 22:04:27 +0100

    Add name validation in employee creation

--- Kotlin/conference-room-booking-api/src/main/kotlin/com/conference/room/booking/api/models/Employee.kt
+++ Kotlin/conference-room-booking-api/src/main/kotlin/com/conference/room/booking/api/models/Employee.kt
@@ -5,10 +5,12 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.Size
 
 @Entity
 data class Employee(
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Int?,
+        @field:Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
         val name: String,
         @field:Email(message = "Email must be valid") val email: String,
         val employer: String,
